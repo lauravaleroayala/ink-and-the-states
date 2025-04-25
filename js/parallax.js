3a19acf318ef4016f64cb1c237e5f1d2c032b22e
lauravaleroayala <lauravalero.92@alumnos.cei.es>
2025-04-25 21:13:40 +0200

    capturas para la memoria

--- js/parallax.js
+++ js/parallax.js
@@ -6,16 +6,19 @@
 
 // escuchamos si es usuario hace scroll en la pantalla
 window.addEventListener('scroll', () => {
-    const parallaxBg = document.querySelector("#parallax__bg");
-    // si el ancho es más grande de 1182 px (igual que media query css)
-    if (window.innerWidth > 1182) {
-      let scrollPos = window.scrollY;
-      //mover el fondo
-      //velocidad 0.3 significa que se mueve al 30% de la velocidad del scroll
-      parallaxBg.style.transform = `translateY(${scrollPos * 0.3}px)`;
-      // si el ancho es más pequeño de 1182 px (igual que media query css)
-    } else {
-        // ponemos el fondo en su posición original
-      parallaxBg.style.transform = 'translateY(0px)';
-    }
-  });
+  const parallaxBg = document.querySelector("#parallax__bg");
+  // si el ancho es más grande de 1182 px (igual que media query css)
+  if (window.innerWidth > 1182) {
+    let scrollPos = window.scrollY;
+    //mover el fondo
+    //velocidad 0.3 significa que se mueve al 30% de la velocidad del scroll
+    parallaxBg.style.transform = `translateY(${scrollPos * 0.3}px)`;
+    // si el ancho es más pequeño de 1182 px (igual que media query css)
+  } else {
+    // ponemos el fondo en su posición original
+    parallaxBg.style.transform = 'translateY(0px)';
+  }
+});
+
+
+    
